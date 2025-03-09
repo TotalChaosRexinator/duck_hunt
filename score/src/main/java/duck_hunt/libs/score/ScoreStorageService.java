0d68dbba4c8d48fe60f1772f19808f70d4c629a4
Spooky_REX <138590414+TotalChaosRexinator@users.noreply.github.com>
2025-03-09 18:18:24 +0100

    Delete score directory

--- score/src/main/java/duck_hunt/libs/score/ScoreStorageService.java
+++
@@ -1,9 +0,0 @@
-package duck_hunt.libs.score;
-
-public interface ScoreStorageService {
-	ScoreList getScoresFromFile() throws Exception;
-	void init();
-	void saveScoresToFile(ScoreList scoreList) throws Exception;
-	void saveScoreToFile(Score score) throws Exception;
-
-}
